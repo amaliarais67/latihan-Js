b148822f56da2b046c845eff2395260e53474bfb
amaliarais67 <amaliarais67@gmail.com>
2022-02-26 15:36:23 +0700

    mennjawab soal algoritma dan pseudocode

---
+++ algoritma.js
@@ -0,0 +1,33 @@
+// untuk menghitung massa jenis 
+/** 
+ * ALGORITMA
+ * buat variabel massa 
+ * buat variabel volume 
+ * buat variabel rho
+ * isi rho dengan rumus massa dibagi volume
+ * cetak luas rho 
+ * 
+ * PSEUDOCODE 
+ * STORE massa WITH ANY NUMBER 
+ * STORE volume WITH ANY NUMBER 
+ * SET rho massa DIVIDED volume
+ * DISPLAY rho 
+*/
+// untuk menghitung luas permukaan tabung 
+/**
+ * ALGORITMA 
+ * buat variabel phi 
+ * isi variabel phi dengan 3.14
+ * buat variabel t => tinggi
+ * buat variabel r => jari-jari 
+ * buat variabel luas_permukaan_tabung 
+ * isi luas_permukaan_tabung dengan rumus buka kurung 2 dikali phi dikali tinggi dikali r tutup kurung di tambah buka kurung 2 dikali phi dikali r dikali r tutup kurung
+ * cetak luas 
+ * 
+ * PSEUDOCODE 
+ * STORE phi WITH 3.14
+ * STORE t WITH ANY NUMBER 
+ * STORE r WITH ANY NUMBER 
+ * SET luas_permukaan_tabung WITH (2 MULTIPLY phi MULTIPLY t MULTIPLY r) ADD (2 MULTIPLY phi MULTIPLY r MULTIPLY r)
+ * DISPLAY luas_permukaan_tabung
+ */
