7c717b6bdda090ef827e6dd72824c3384e882305
amaliarais67 <amaliarais67@gmail.com>
2022-02-26 16:21:31 +0700

    menjawab soal variabel

---
+++ variabel.js
@@ -0,0 +1,30 @@
+// menghitung volume setengan bola 
+/**
+ * ALGORITMA 
+ * buat variabel phi
+ * isi variabel phi dengan 3.14 
+ * buat variabel r => jari-jari 
+ * buat variabel v => volume 
+ * isi variabel v dengan rumus 1 dibagi 2 dikali phi dikali r dikali r dikali r 
+ * cetak v 
+ * 
+ * PSEUDOCODE 
+ * STORE phi WITH 3.14 
+ * STORE r WITH ANY NUMBER
+ * SET v WITH 1 DIVIDED 3 MULTIPLY phi MULTIPLY phi MULTIPLY r MULTIPLY r MULTIPLY r  
+ */
+// CODING 1
+// var phi = 3.14; 
+// var r = 2;
+// var v = 1/2 * phi * r * r * r ;
+// console.log(v);
+// CODING 2
+// var s = "Selamat belajar, Lia!";
+// console.log(s);
+// CODING 3
+var nama = "lia";
+var status = true;
+Math.random();
+var number = Math.floor(Math.random()*10); 
+var output = "menampilkan generate angka :" + nama + number + status;
+console.log(output);
