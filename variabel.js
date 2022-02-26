f21412f749e1a43daa95f4962a01d0ee69a3f0b6
amaliarais67 <amaliarais67@gmail.com>
2022-02-26 16:28:26 +0700

    menghapus tanda coment pada kodingan

--- variabel.js
+++ variabel.js
@@ -14,13 +14,13 @@
  * SET v WITH 1 DIVIDED 3 MULTIPLY phi MULTIPLY phi MULTIPLY r MULTIPLY r MULTIPLY r  
  */
 // CODING 1
-// var phi = 3.14; 
-// var r = 2;
-// var v = 1/2 * phi * r * r * r ;
-// console.log(v);
+var phi = 3.14; 
+var r = 2;
+var v = 1/2 * phi * r * r * r ;
+console.log(v);
 // CODING 2
-// var s = "Selamat belajar, Lia!";
-// console.log(s);
+var s = "Selamat belajar, Lia!";
+console.log(s);
 // CODING 3
 var nama = "lia";
 var status = true;
